df0b574cbf178f5c046e79f729e592a45bad5e4e
Loshik <143479051+Loshik@users.noreply.github.com>
2023-09-04 16:42:00 +0530

    Add files via upload

---
+++ Arithmetic Operations.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	printf("%d",c);
+	printf("%d",d);
+	printf("%d",e);
+	printf("%d",f);
+	return 0;
+}
+
---
+++ Percentage of a Student in  five subjects.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+	f=(a+b+c+d+e)/5;
+	printf("%d",f);
+	return 0;
+}
